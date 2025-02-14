ef76b341ecb83ac04a44e16df477de2b13a4f0db
Lera <lera_muza00@mail.ru>
2025-02-14 14:36:41 +0300

    install linter one more time

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
 ];
-
-module.exports = {
-  env: {
-    node: true,
-    jest: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:jest/recommended',
-  ],
-  globals: {
-    test: true,
-    expect: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-};
