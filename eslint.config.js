490e2811b4258ade178a9bacb4451060ddea7af4
Lera <lera_muza00@mail.ru>
2025-02-19 18:16:00 +0300

    install eslint again

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,9 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
-module.exports = {
-  env: {
-    jest: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:jest/recommended',
-  ],
-  plugins: ['jest'],
-};
